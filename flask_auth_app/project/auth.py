c3a8a3e954924cb80251ab351ca55f53a040c684
Micky373 <michaeltamirie@gmail.com>
2022-06-18 13:01:56 +0300

    Edit the authorization python file for signing up with a single email

--- flask_auth_app/project/auth.py
+++ flask_auth_app/project/auth.py
@@ -1,5 +1,5 @@
 from crypt import methods
-from flask import Blueprint, render_template, redirect, url_for,request
+from flask import Blueprint, render_template, redirect, url_for,request,flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
@@ -24,6 +24,7 @@ def signup_post():
     user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
 
     if user: # if a user is found, we want to redirect back to signup page so user can try again
+        flash('Email already exists')
         return redirect(url_for('auth.signup'))
 
     # create a new user with the form data. Hash the password so the plaintext version isn't saved.
